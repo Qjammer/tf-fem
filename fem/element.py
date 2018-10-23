b36027602793a19848b5730e31d1f979499933bf
Qjammer <baladoramiro+github@gmail.com>
2018-10-23 14:11:16 +0200

    basics of element integration

--- fem/element.py
+++ fem/element.py
@@ -2,32 +2,50 @@ import tensorflow as tf
 import math
 
 class BaseElement:
-	self.nodes
-	def __init__(self):
-		pass
+	def __init__(self,nodes,stdElement):
+		self.nodes=nodes
+		self.stdElement=stdElement
+
 	def jacobianMatrix(self):
 		pass
 
+	def getMatrix(self,N_dims,B_dims,N_vertex,B_vertex):
+		return self.jacobianMatrix()*self.stdElement.getMatrix(N_dims,B_dims,N_vertex,B_vertex)
+
 class TetrahedronElement(BaseElement):
 	def __init__(self):
+		super(TetrahedronElement,self).__init__(nodes,TetrahedronStdElement())
 		pass
+
 	def jacobianMatrix(self):
-		pass
+		return tf.transpose(nodes[1:]-nodes[0])
+
 
 
 class BaseStdElement:
 	def __init__(self):
 		pass
-	def integral(self,N_dims,B_dims,N_vertex,B_vertex):
+	def getMatrix(self,N_dims,B_dims,N_vertex,B_vertex):
 		pass
 
 class TetrahedronStdElement(BaseStdElement):
 
 	def __init__(self):
+		super(TetrahedronStfElement,self).__init__()
 		pass
 
-	def integral(self,N_dims,B_dims,N_vertex,B_vertex):
-		#if dimensions are repeated, they are summed over. Vertex 0 is origin, others are base vectors
+	def getBasePolynomials(self,order):
+		if(order!=1): raise ValueError("Element orders different from 1 are not implemented")
+		return tf.constant([[[1,-1],[-1,0]],\
+							[[-1,0],[0,0]]])
+		
+
+	def getMatrix(self,N_dims,B_dims,N_vertex,B_vertex):
+		# If dimensions are repeated, they are summed over. Vertex 0 is origin, others are base vectors
+		# 1. Generate polynomials Ns and Ni
+		# 2. Compute product of polynomials NsNi
+		# 3. Calculate integral of each polynomial
+		# 4. Return matrix of integral of polynomials
 		pass
 
 	def monomer_integral_3simplex(self,nx,ny,nz):
