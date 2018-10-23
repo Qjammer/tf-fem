11882e14385defeb2a61038ebbd48839bb0abd2b
Qjammer <baladoramiro+github@gmail.com>
2018-10-23 12:21:16 +0200

    basic element functions

---
+++ fem/element.py
@@ -0,0 +1,33 @@
+import tensorflow as ts
+
+class BaseElement:
+	nodes
+	def __init__(self):
+		pass
+	def jacobianMatrix():
+		pass
+
+class TetrahedronElement(base_element):
+	def __init__():
+		pass
+	def jacobianMatrix():
+		pass
+
+
+class BaseStdElement:
+	def __init__():
+		pass
+	def integral(N_dims,B_dims,N_vertex,B_vertex):
+		pass
+
+class TetrahedronStdElement(base_std_element):
+
+	def __init__():
+		pass
+
+	def integral(N_dims,B_dims,N_vertex,B_vertex):
+		#if dimensions are repeated, they are summed over. Vertex 0 is origin, others are base vectors
+		pass
+
+	def monomer_integral_3simplex(nx,ny,nz):
+		return math.factorial(nx)*math.factorial(ny)*math.factorial(nz)/math.factorial(3+nx+ny+nz)
